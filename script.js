82ae14ecf2c77dddc98a0f62a42642944e61a76b
veltonbo <eliveltonoliveiranbo@gmail.com>
2025-04-29 07:15:00 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Código completo JavaScript: Firebase, funções de inicialização, menus, Aplicações, Tarefas, Financeiro, Colheita, Relatórios
+
+// firebaseConfig + inicialização
+// função mostrarAba()
+// alternarTema()
+// adicionarAplicacao(), atualizarAplicacoes(), carregarAplicacoes()
+// adicionarTarefa(), atualizarTarefas(), carregarTarefas()
+// adicionarFinanceiro(), atualizarFinanceiro(), carregarFinanceiro()
+// Funções de colheita, geração de gráficos, exportações, etc.

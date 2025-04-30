006c1b59a847c6f2618b956362ce9527506b6fda
veltonbo <eliveltonoliveiranbo@gmail.com>
2025-04-29 21:51:14 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Firebase
+// Firebase Config
 const firebaseConfig = {
   apiKey: "AIzaSyD773S1h91tovlKTPbaeAZbN2o1yxROcOc",
   authDomain: "manej-cafe.firebaseapp.com",
@@ -8,6 +8,8 @@ const firebaseConfig = {
   messagingSenderId: "808931200634",
   appId: "1:808931200634:web:71357af2ff0dc2e4f5f5c3"
 };
+
+// Inicializar Firebase
 firebase.initializeApp(firebaseConfig);
 const db = firebase.database();
 

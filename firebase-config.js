f48b6f218914922b84b9f82e9012d43b3a3a2705
veltonbo <eliveltonoliveiranbo@gmail.com>
2025-05-02 17:17:17 -0400

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// ===== Firebase Config =====
+const firebaseConfig = {
+  apiKey: "AIzaSyD773S1h91tovlKTPbaeAZbN2o1yxROcOc",
+  authDomain: "manej-cafe.firebaseapp.com",
+  databaseURL: "https://manej-cafe-default-rtdb.firebaseio.com",
+  projectId: "manej-cafe",
+  storageBucket: "manej-cafe.appspot.com",
+  messagingSenderId: "808931200634",
+  appId: "1:808931200634:web:71357af2ff0dc2e4f5f5c3"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();

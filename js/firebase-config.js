95f908c144281edb168077024fdcdd51152e4e55
veltonbo <eliveltonoliveiranbo@gmail.com>
2025-05-09 15:50:29 -0400

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,25 +1,20 @@
-<!-- Firebase (Compatível com versões sem módulos) -->
-<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-database-compat.js"></script>
+// js/firebase-config.js
 
-<script>
-  // Configurações do Firebase
-  const firebaseConfig = {
-    apiKey: "AIzaSyD773S1h91tovlKTPbaeAZbN2o1yxROcOc",
-    authDomain: "manej-cafe.firebaseapp.com",
-    databaseURL: "https://manej-cafe-default-rtdb.firebaseio.com",
-    projectId: "manej-cafe",
-    storageBucket: "manej-cafe.appspot.com", // Corrigido o storageBucket
-    messagingSenderId: "808931200634",
-    appId: "1:808931200634:web:71357af2ff0dc2e4f5f5c3"
-  };
+// Configurações do Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyD773S1h91tovlKTPbaeAZbN2o1yxROcOc",
+  authDomain: "manej-cafe.firebaseapp.com",
+  databaseURL: "https://manej-cafe-default-rtdb.firebaseio.com",
+  projectId: "manej-cafe",
+  storageBucket: "manej-cafe.appspot.com", // Corrigido o storageBucket
+  messagingSenderId: "808931200634",
+  appId: "1:808931200634:web:71357af2ff0dc2e4f5f5c3"
+};
 
-  // Inicializa o Firebase
-  if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
-  }
+// Inicializa o Firebase
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 
-  const db = firebase.database();
-  console.log("Firebase inicializado corretamente.");
-</script>
-
+const db = firebase.database();
+console.log("Firebase inicializado corretamente.");

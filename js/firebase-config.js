f6be9f401fce329275ac2dc65a9879b8585aac90
veltonbo <eliveltonoliveiranbo@gmail.com>
2025-05-09 15:24:12 -0400

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,17 +1,19 @@
-// ===== Firebase Config =====
-const firebaseConfig = {
-  apiKey: "AIzaSyD773S1h91tovlKTPbaeAZbN2o1yxROcOc",
-  authDomain: "manej-cafe.firebaseapp.com",
-  databaseURL: "https://manej-cafe-default-rtdb.firebaseio.com",
-  projectId: "manej-cafe",
-  storageBucket: "manej-cafe.appspot.com",
-  messagingSenderId: "808931200634",
-  appId: "1:808931200634:web:71357af2ff0dc2e4f5f5c3"
-};
+// js/firebase-config.js
 
-firebase.initializeApp(firebaseConfig);
-const db = firebase.database();
-
+// Verifica se o Firebase já foi inicializado para evitar erros
 if (!firebase.apps.length) {
+  const firebaseConfig = {
+    apiKey: "AIzaSyD735HjSt1yolt1X7RbsaeZN2oIyxRDcOc",
+    authDomain: "manejo-cafe.firebaseapp.com",
+    databaseURL: "https://manejo-cafe-default-rtdb.firebaseio.com",
+    projectId: "manejo-cafe",
+    storageBucket: "manejo-cafe.appspot.com",
+    messagingSenderId: "808391206364",
+    appId: "1:808391206364:web:1537f2dfcbdc2e4f5fc5c3"
+  };
+
   firebase.initializeApp(firebaseConfig);
 }
+
+// Garante que o database seja inicializado corretamente
+const db = firebase.database().ref("aplicacoes");

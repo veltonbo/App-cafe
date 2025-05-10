ff75824e9ff85597099040cfb25d6039d2a61782
veltonbo <eliveltonoliveiranbo@gmail.com>
2025-05-09 20:24:39 -0400

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,20 +1,14 @@
-// js/firebase-config.js
-
-// Configurações do Firebase
+// ===== CONFIGURAÇÃO DO FIREBASE =====
 const firebaseConfig = {
   apiKey: "AIzaSyD773S1h91tovlKTPbaeAZbN2o1yxROcOc",
   authDomain: "manej-cafe.firebaseapp.com",
   databaseURL: "https://manej-cafe-default-rtdb.firebaseio.com",
   projectId: "manej-cafe",
-  storageBucket: "manej-cafe.appspot.com", // Corrigido o storageBucket
+  storageBucket: "manej-cafe.appspot.com",
   messagingSenderId: "808931200634",
   appId: "1:808931200634:web:71357af2ff0dc2e4f5f5c3"
 };
 
-// Inicializa o Firebase
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
-
+// ===== INICIALIZAÇÃO DO FIREBASE =====
+firebase.initializeApp(firebaseConfig);
 const db = firebase.database();
-console.log("Firebase inicializado corretamente.");

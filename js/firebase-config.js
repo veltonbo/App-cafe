929499acf9452155f33a9bfdea1784de77e9c7b7
veltonbo <eliveltonoliveiranbo@gmail.com>
2025-05-09 22:04:58 -0400

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,20 +1,25 @@
-// ===== Firebase Config =====
-const firebaseConfig = {
-  apiKey: "AIzaSyD773S1h91tovlKTPbaeAZbN2o1yxROcOc",
-  authDomain: "manej-cafe.firebaseapp.com",
-  databaseURL: "https://manej-cafe-default-rtdb.firebaseio.com",
-  projectId: "manej-cafe",
-  storageBucket: "manej-cafe.appspot.com",
-  messagingSenderId: "808931200634",
-  appId: "1:808931200634:web:71357af2ff0dc2e4f5f5c3"
-};
+// ===== CONFIGURAÇÃO DO FIREBASE =====
 
-firebase.initializeApp(firebaseConfig);
-const db = firebase.database();
+// Importa os módulos necessários do Firebase
+document.addEventListener("DOMContentLoaded", () => {
+  if (typeof firebase === "undefined") {
+    alert("Erro: Firebase não está carregado corretamente.");
+    return;
+  }
 
-if (!firebase.apps.length) {
+  // Configurações do Firebase
+  const firebaseConfig = {
+    apiKey: "AIzaSyD773S1h91tovlKTPbaeAZbN2o1yxROcOc",
+    authDomain: "manej-cafe.firebaseapp.com",
+    databaseURL: "https://manej-cafe-default-rtdb.firebaseio.com",
+    projectId: "manej-cafe",
+    storageBucket: "manej-cafe.appspot.com",
+    messagingSenderId: "808931200634",
+    appId: "1:808931200634:web:71357af2ff0dc2e4f5f5c3"
+  };
+
+  // Inicializa o Firebase
   firebase.initializeApp(firebaseConfig);
-}
 
   // Verifica a conexão com o Firebase Realtime Database
   const db = firebase.database();

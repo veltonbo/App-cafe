c4760498fe44717bb2f8b207341b1b6f2d0849e2
veltonbo <eliveltonoliveiranbo@gmail.com>
2025-05-12 15:50:58 -0400

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,4 +1,4 @@
-// ===== Firebase Config =====
+// Deixe apenas:
 const firebaseConfig = {
   apiKey: "AIzaSyD773S1h91tovlKTPbaeAZbN2o1yxROcOc",
   authDomain: "manej-cafe.firebaseapp.com",
@@ -9,13 +9,7 @@ const firebaseConfig = {
   appId: "1:808931200634:web:71357af2ff0dc2e4f5f5c3"
 };
 
-// Inicializa o Firebase (apenas se ainda não estiver inicializado)
 if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
 }
-
 const db = firebase.database();
-const auth = firebase.auth();
-
-// Exportar para uso em outros arquivos
-export { db, auth };

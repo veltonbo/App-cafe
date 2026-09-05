2370df2d9ba0865642d08b57985e4f251f87d2e0
veltonbo <eliveltonoliveiranbo@gmail.com>
2026-09-05 15:04:18 -0400

    fix: habilitar compilador Vite do Vercel Workflow

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
-export default {
+import { defineConfig } from 'vite';
+import { workflow } from 'workflow/vite';
+
+export default defineConfig({
   root: '.',
+  plugins: [workflow()],
   server: {
     open: true
   }
-};
+});

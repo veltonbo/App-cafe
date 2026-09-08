86561037eaad2a5f9920744a02243e2c2175ddd3
veltonbo <eliveltonoliveiranbo@gmail.com>
2026-09-08 11:52:44 -0400

    Corrigir metodos CORS das APIs

--- server/api/_tuya.js
+++ server/api/_tuya.js
@@ -23,7 +23,7 @@ export function applyCors(req, res) {
   if (origin && allowed.has(origin)) res.setHeader('Access-Control-Allow-Origin', origin);
   res.setHeader('Vary', 'Origin');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PATCH,PUT,DELETE,OPTIONS');
 }
 
 export function authorize(req, res) {

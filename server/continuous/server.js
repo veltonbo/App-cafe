691ac343fb5a198c17e7b1d8c4df0a630375e76c
veltonbo <eliveltonoliveiranbo@gmail.com>
2026-09-08 14:14:04 -0400

    Publicar correcao do alerta de atraso

--- server/continuous/server.js
+++ server/continuous/server.js
@@ -14,6 +14,7 @@ import { runViveiroWeatherCheck, getViveiroWeatherConfig } from '../api/viveiro/
 import { enforceViveiroInterlocks } from '../api/viveiro/_interlock.js';
 
 const PORT=Math.max(1,Number(process.env.PORT||3000));
+// UI cycle-alert freshness rollout v1
 // Automatico 2.0 final rollout v1
 // Publish marker: climate auto v8
 // Publish marker: viveiro full intelligence v7

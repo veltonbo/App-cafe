5f6eaa4fd929f23175eeb23d386b28183050aef9
veltonbo <eliveltonoliveiranbo@gmail.com>
2026-09-09 15:13:44 -0400

    Atualizar cache do layout enxuto

--- scripts/inject-irrigacao-ui.js
+++ scripts/inject-irrigacao-ui.js
@@ -14,14 +14,14 @@ html=html.split('\n').filter(line=>
   !line.includes('ui-shell.js')&&
   !line.includes('ui-mockup-v2.js')
 ).join('\n');
-const css='<link rel="stylesheet" href="/irrigacao/ui-professional.css?v=20260909-17">\n'+
+const css='<link rel="stylesheet" href="/irrigacao/ui-professional.css?v=20260909-18">\n'+
   '<link rel="stylesheet" href="/irrigacao/ui-premium.css?v=20260909-1">\n'+
-  '<link rel="stylesheet" href="/irrigacao/ui-shell.css?v=20260909-6">\n'+
-  '<link rel="stylesheet" href="/irrigacao/ui-mockup-v2.css?v=20260909-3">';
+  '<link rel="stylesheet" href="/irrigacao/ui-shell.css?v=20260909-7">\n'+
+  '<link rel="stylesheet" href="/irrigacao/ui-mockup-v2.css?v=20260909-4">';
 const js='<script src="/irrigacao/live-runtime.js?v=20260909-5" defer></script>\n'+
-  '<script src="/irrigacao/ui-professional.js?v=20260909-18" defer></script>\n'+
-  '<script src="/irrigacao/ui-shell.js?v=20260909-6" defer></script>\n'+
-  '<script src="/irrigacao/ui-mockup-v2.js?v=20260909-3" defer></script>';
+  '<script src="/irrigacao/ui-professional.js?v=20260909-19" defer></script>\n'+
+  '<script src="/irrigacao/ui-shell.js?v=20260909-7" defer></script>\n'+
+  '<script src="/irrigacao/ui-mockup-v2.js?v=20260909-4" defer></script>';
 html=html.replace('</head>',css+'\n</head>');
 html=html.replace('</body>',js+'\n</body>');
 fs.writeFileSync(file,html);

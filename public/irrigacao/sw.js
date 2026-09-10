27ad6f7d7819e49e054598e7ec4a09d38072346c
veltonbo <eliveltonoliveiranbo@gmail.com>
2026-09-09 22:05:09 -0400

    Atualizar PWA para precisão corrigida do Viveiro

--- public/irrigacao/sw.js
+++ public/irrigacao/sw.js
@@ -1,8 +1,8 @@
-const CACHE='fazenda2e-viveiro-clean-v4';
+const CACHE='fazenda2e-viveiro-clean-v5';
 const SHELL=[
   '/irrigacao/',
   '/irrigacao/app.css?v=20260909-3',
-  '/irrigacao/app.js?v=20260909-4',
+  '/irrigacao/app.js?v=20260909-5',
   '/irrigacao/manifest.webmanifest',
   '/irrigacao/icon.svg'
 ];

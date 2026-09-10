9c5f53c8c2310b08ec9a225512b2755a83830c6d
veltonbo <eliveltonoliveiranbo@gmail.com>
2026-09-09 22:25:09 -0400

    Atualizar PWA para sessão segura do Viveiro

--- public/irrigacao/sw.js
+++ public/irrigacao/sw.js
@@ -1,8 +1,8 @@
-const CACHE='fazenda2e-viveiro-clean-v5';
+const CACHE='fazenda2e-viveiro-clean-v6';
 const SHELL=[
   '/irrigacao/',
   '/irrigacao/app.css?v=20260909-3',
-  '/irrigacao/app.js?v=20260909-5',
+  '/irrigacao/app.js?v=20260909-6',
   '/irrigacao/manifest.webmanifest',
   '/irrigacao/icon.svg'
 ];

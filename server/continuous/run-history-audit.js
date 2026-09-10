4508a67c5f07fe988e7349dab12dcad61d7ee11a
veltonbo <eliveltonoliveiranbo@gmail.com>
2026-09-10 08:13:35 -0400

    Finalizar manutenção do histórico do Viveiro
    * Restaurar inicialização normal após reparo
    
    * Remover auditoria temporária do histórico
    
    * Remover executor temporário da auditoria
    
    * Remover reparo temporário após conclusão

--- server/continuous/run-history-audit.js
+++
@@ -1,8 +0,0 @@
-import { auditViveiroHistoryReadOnly } from './history-audit.js';
-
-try{
-  await auditViveiroHistoryReadOnly();
-}catch(error){
-  console.error('VIVEIRO_HISTORY_AUDIT_ERROR',error?.stack||error?.message||String(error));
-  process.exitCode=1;
-}

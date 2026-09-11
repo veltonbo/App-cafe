995113a7f9f3f4b989f7bd367b58b8a5558239b9
veltonbo <eliveltonoliveiranbo@gmail.com>
2026-09-11 07:32:34 -0400

    Refresh public irrigation PWA cache for climate forecast

--- public/irrigacao/sw.js
+++ public/irrigacao/sw.js
@@ -1,8 +1,8 @@
-const CACHE='fazenda2e-viveiro-clean-v10';
+const CACHE='fazenda2e-viveiro-clean-v11';
 const SHELL=[
   '/irrigacao/',
-  '/irrigacao/app.css?v=20260911-6',
-  '/irrigacao/app.js?v=20260911-6',
+  '/irrigacao/app.css?v=20260911-7',
+  '/irrigacao/app.js?v=20260911-7',
   '/irrigacao/manifest.webmanifest',
   '/irrigacao/icon.svg'
 ];
@@ -13,38 +13,28 @@ self.addEventListener('install',event=>{
 });
 
 self.addEventListener('activate',event=>{
-  event.waitUntil(
-    caches.keys().then(keys=>Promise.all(
-      keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))
-    ))
-  );
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))));
   self.clients.claim();
 });
 
 self.addEventListener('fetch',event=>{
   const url=new URL(event.request.url);
   if(url.pathname.startsWith('/api/'))return;
-
   if(event.request.mode==='navigate'){
-    event.respondWith(
-      fetch(event.request,{cache:'no-store'}).then(response=>{
-        if(url.pathname==='/irrigacao/'||url.pathname==='/irrigacao'){
-          const copy=response.clone();
-          caches.open(CACHE).then(cache=>cache.put('/irrigacao/',copy)).catch(()=>null);
-        }
-        return response;
-      }).catch(()=>caches.match('/irrigacao/'))
-    );
+    event.respondWith(fetch(event.request,{cache:'no-store'}).then(response=>{
+      if(url.pathname==='/irrigacao/'||url.pathname==='/irrigacao'){
+        const copy=response.clone();
+        caches.open(CACHE).then(cache=>cache.put('/irrigacao/',copy)).catch(()=>null);
+      }
+      return response;
+    }).catch(()=>caches.match('/irrigacao/')));
     return;
   }
-
   if(url.origin===self.location.origin&&url.pathname.startsWith('/irrigacao/')){
-    event.respondWith(
-      fetch(event.request,{cache:'no-store'}).then(response=>{
-        const copy=response.clone();
-        caches.open(CACHE).then(cache=>cache.put(event.request,copy)).catch(()=>null);
-        return response;
-      }).catch(()=>caches.match(event.request))
-    );
+    event.respondWith(fetch(event.request,{cache:'no-store'}).then(response=>{
+      const copy=response.clone();
+      caches.open(CACHE).then(cache=>cache.put(event.request,copy)).catch(()=>null);
+      return response;
+    }).catch(()=>caches.match(event.request)));
   }
 });
